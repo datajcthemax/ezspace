a0b0e079187fdf3ae95aebc7703ae6ff504f9a3c
Cho Jai Ho <128767460+datajcthemax@users.noreply.github.com>
2023-10-21 01:45:11 +0900

    next-auth

---
+++ app/components/Navbar.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+function Navbar() {
+  return (
+    <nav className="bg-black p-4 text-white">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <span className="cursor-pointer text-2xl font-bold">EzSpace</span>
+          </Link>
+          <Link href="/login">
+            <span className="cursor-pointer">로그인</span>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
+
 export default function Home() {
   return (
     <div>
-     home
+      home
     </div>
   )
 }
